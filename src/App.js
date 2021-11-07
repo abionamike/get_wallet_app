8445d1cd4d2184c62179bf0887090707dc80a31d
Michael Abiona <amabiona21@gmail.com>
2021-11-07 12:54:20 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/navbar';
 import TransactionHistory from './pages/transactionHistory';
 import SignIn from './pages/signIn';
 import Home from './pages/home';
@@ -9,7 +7,6 @@ import Home from './pages/home';
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <main className="container">
         <Routes>
           <Route exact path="/" element={<Home />} />
